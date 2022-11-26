3bf55b612bebe63dd8c8e5b59636eec3764f3f07
pava2n <117650964+pava2n@users.noreply.github.com>
2022-11-27 01:02:08 +0530

    when exception occured

---
+++ when.exception occured.java
@@ -0,0 +1,18 @@
+class pavan{
+    public static void main(String args[]){
+        int fNumber= 25;
+        int sNumber=0;
+        int result;
+         
+           result= fNumber/sNumber;
+           
+           System.out.println(result);
+    }
+}
+
+
+ output;
+    
+        java -cp /tmp/sfdx46BzPv pavan
+Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at pavan.main(pavan.java:7)
